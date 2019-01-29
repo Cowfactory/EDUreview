2195c84e01c6f379e5923437d99c2e1f4939b018
Cowfactory <hhongnr@gmail.com>
2019-01-28 19:39:40 -0800

    updated api routes w/ HTTP status codes

--- routes/api/institutions.js
+++ routes/api/institutions.js
@@ -1,14 +1,31 @@
 const router = require('express').Router();
 const Institution = require('../../models/Institution');
 
-/* --- Adds a new institution to db -- */
+/* --- Adds a new institution to db --- */
 router.post('/', (req, res) => {
-
+    let institution = new Institution({
+        name: req.body.name,
+        website: req.body.websiteURL
+    })
+    institution.save(err => {
+        if(err) return err;
+        res.status(201).send({
+            msg: "Institution Entry Added"
+        })
+    })
 });
 
-/* --- Gets all institutions from db -- */
+/* --- Gets all institutions from db --- */
 router.get('/', (req, res) => {
- 
+    Institution.find({}, (err, result) => {
+        if(err) {
+            res.status(500).send({
+                msg: "Internal Error"
+            })
+            return err;
+        }
+        res.status(200).send(JSON.stringify(result));
+    });  
 });
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const User = require('../../models/User');
 
-/* --- Adds a new user to db -- */
+/* --- Adds a new user to db --- */
 router.post('/', (req, res) => {
 
 });
 
-/* --- Gets all users from db -- */
+/* --- Gets all users from db --- */
 router.get('/', (req, res) => {
  
 });
