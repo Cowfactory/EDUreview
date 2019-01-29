e26e3d1ac1f7ff90e61a42ab8f9e792523e82716
zhangrom000 <roman.g.zhang@gmail.com>
2019-01-28 19:03:22 -0800

    Renames AddInstitution.jsx to AddInstitutionPage.jsx

---
+++ client/src/pages/AddInstitutionPage/AddInstitutionPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTemplate from '../../templates/PageTemplate/PageTemplate';
+
+function AddInstitutionPage(props) {
+    return (
+        <PageTemplate >
+            <h1>Add New Institution to EDUreview</h1>
+        </PageTemplate>
+    )
+}
+
+export default AddInstitutionPage;
