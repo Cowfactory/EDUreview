737d6c530ec0e2f3b62acf19fdb179f29a683746
Cowfactory <hhongnr@gmail.com>
2019-02-28 13:56:25 -0800

    fixed institution details link

--- client/src/components/InstitutionSearchResultsEntry/InstitutionSearchResultsEntry.jsx
+++ client/src/components/InstitutionSearchResultsEntry/InstitutionSearchResultsEntry.jsx
@@ -5,7 +5,7 @@ import "./InstitutionSearchResultsEntry.css";
 function InstitutionSearchResultsEntry(props) {
     let { name, id } = props;
 
-    let linkUrl = `/programs/${id}`;
+    let linkUrl = `/institutions/${id}`;
     return (
         <Link to={linkUrl}>
             <div className="InstitutionSearchResultsEntry">
