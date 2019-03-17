926f26264a5a2fd022c090f314a968337b202ba3
Cowfactory <hhongnr@gmail.com>
2019-03-16 19:56:35 -0700

    Styling

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -4,6 +4,8 @@ import { AppConsumer } from '../../App/AppContext';
 import { Redirect } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import FormTemplate from '../../templates/FormTemplate/FormTemplate';
 
 class LoginPage extends React.Component {
     state = {
@@ -48,43 +50,46 @@ class LoginPage extends React.Component {
                 {({ login }) => {
                     return (
                         <PageTemplate>
-                            <Form
-                                noValidate
-                                validated={validated}
-                                onSubmit={e => this.handleSubmit(e, login)}
-                            >
-                                <Form.Group controlId="formGroupEmail">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control
-                                        type="email"
-                                        required
-                                        placeholder="Enter Email"
-                                        name="email"
-                                        value={this.state.email}
-                                        onChange={this.handleChange}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        Required Field
-                                    </Form.Control.Feedback>
-                                </Form.Group>
+                            <FormTemplate>
 
-                                <Form.Group controlId="formGroupPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control
-                                        type="password"
-                                        required
-                                        placeholder="Enter Password"
-                                        name="password"
-                                        value={this.state.password}
-                                        isInvalid={!!errors.msg}
-                                        onChange={this.handleChange}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {errors.msg || "Username or Password was invalid"}
+                                <Form
+                                    noValidate
+                                    validated={validated}
+                                    onSubmit={e => this.handleSubmit(e, login)}
+                                >
+                                    <Form.Group controlId="formGroupEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            required
+                                            placeholder="Enter Email"
+                                            name="email"
+                                            value={this.state.email}
+                                            onChange={this.handleChange}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Required Field
                                     </Form.Control.Feedback>
-                                </Form.Group>
-                                <Button type="submit">Submit</Button>
-                            </Form>
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formGroupPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            required
+                                            placeholder="Enter Password"
+                                            name="password"
+                                            value={this.state.password}
+                                            isInvalid={!!errors.msg}
+                                            onChange={this.handleChange}
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            {errors.msg || "Username or Password was invalid"}
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
+                                    <Button type="submit">Submit</Button>
+                                </Form>
+                            </FormTemplate>
                         </PageTemplate>
                     );
                 }}
