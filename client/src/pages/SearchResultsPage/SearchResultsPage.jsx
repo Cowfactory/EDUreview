986955db96ad8ccc1f28d6261e70867cfb053608
Cowfactory <hhongnr@gmail.com>
2019-03-18 18:17:44 -0700

    in progress commit

--- client/src/pages/SearchResultsPage/SearchResultsPage.jsx
+++ client/src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -4,7 +4,7 @@ import ProgramSearchResultsEntry from '../../components/ProgramSearchResultsEntr
 import InstitutionSearchResultsEntry from '../../components/InstitutionSearchResultsEntry/InstitutionSearchResultsEntry';
 import PageTemplate from '../../templates/PageTemplate/PageTemplate';
 import styles from './SearchResultsPage.module.css';
-import { Link, Redirect } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom';
 import { RegionDropdown } from 'react-country-region-selector';
 
 const DEFAULT_SHOW = 10;
@@ -19,12 +19,12 @@ class SearchResultsPage extends React.Component {
         stateCode: '',
         ascending: 1,
         redirect: false,
-        type: "programs",
-        query: ""
+        type: 'programs',
+        query: ''
     };
 
     componentDidMount() {
-        document.title = "Search Results - EDUreview";
+        document.title = 'Search Results - EDUreview';
 
         let type, query;
         try {
@@ -36,7 +36,7 @@ class SearchResultsPage extends React.Component {
         }
         this.setState({ type, query }, () => {
             this.queryForResults(DEFAULT_SHOW, DEFAULT_SKIP);
-        })
+        });
     }
 
     queryForResults = (show, skip, stateCode, ascending) => {
@@ -56,6 +56,7 @@ class SearchResultsPage extends React.Component {
         })
             .then(response => response.json())
             .then(res => {
+                console.log(res);
                 this.setState({
                     results: res.results || [],
                     skip: Number(skip),
@@ -64,43 +65,58 @@ class SearchResultsPage extends React.Component {
                     stateCode,
                     ascending
                 });
-            })
-    }
+            });
+    };
 
-    handleShowChange = (e) => {
-        this.queryForResults(e.target.value, this.state.skip, this.state.stateCode, this.state.ascending);
-    }
+    handleShowChange = e => {
+        this.queryForResults(
+            e.target.value,
+            this.state.skip,
+            this.state.stateCode,
+            this.state.ascending
+        );
+    };
 
-    showPreviousPage = (e) => {
+    showPreviousPage = e => {
         let newSkip;
         if (this.state.skip - this.state.show <= 0) {
             newSkip = 0;
         } else {
-            newSkip = Number(this.state.skip) - Number(this.state.show)
+            newSkip = Number(this.state.skip) - Number(this.state.show);
         }
 
         this.queryForResults(this.state.show, newSkip, this.state.stateCode, this.state.ascending);
-    }
+    };
 
-    showNextPage = (e) => {
+    showNextPage = e => {
         if (this.state.results.length === 0) {
             return; //disable button when there are no more results
         }
-        let newSkip = Number(this.state.skip) + Number(this.state.show)
+        let newSkip = Number(this.state.skip) + Number(this.state.show);
         this.queryForResults(this.state.show, newSkip, this.state.stateCode, this.state.ascending);
-    }
+    };
 
-    resetControls = (e) => {
-        this.queryForResults(DEFAULT_SHOW, DEFAULT_SKIP, this.state.stateCode, this.state.ascending);
-    }
-    selectstateCode = (e) => {
+    resetControls = e => {
+        this.queryForResults(
+            DEFAULT_SHOW,
+            DEFAULT_SKIP,
+            this.state.stateCode,
+            this.state.ascending
+        );
+    };
+    selectstateCode = e => {
         this.queryForResults(this.state.show, DEFAULT_SKIP, e, this.state.ascending);
-    }
-    handleSortChange = (e) => {
-        this.queryForResults(this.state.show, this.state.skip, this.state.stateCode, e.target.value);
-    }
+    };
+    handleSortChange = e => {
+        this.queryForResults(
+            this.state.show,
+            this.state.skip,
+            this.state.stateCode,
+            e.target.value
+        );
+    };
     render() {
-        if (this.state.redirect) return <Redirect to="/" />
+        if (this.state.redirect) return <Redirect to="/" />;
         let { type, query } = this.state;
 
         let haveResults = this.state.results.length > 0 ? true : false;
@@ -123,72 +139,102 @@ class SearchResultsPage extends React.Component {
             resultsList = <p>No Results Found</p>;
         }
 
-        return <PageTemplate>
-            <div className={styles.filter_toolbox}>
-                Filter Tools:
-                <div>
-                    <p>Show Only:
-                        <RegionDropdown
-                            country={"United States"}
-                            value={this.state.stateCode}
-                            onChange={this.selectstateCode}
-                            valueType="short"
-                        />
-                    </p>
+        return (
+            <PageTemplate>
+                <div className={styles.filter_toolbox}>
+                    Filter Tools:
+                    <div>
+                        <p>
+                            Show Only:
+                            <RegionDropdown
+                                country={'United States'}
+                                value={this.state.stateCode}
+                                onChange={this.selectstateCode}
+                                valueType="short"
+                            />
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Sort By Names:
+                            <select onChange={this.handleSortChange}>
+                                <option value={1}>Ascending</option>
+                                <option value={-1}>Descending</option>
+                            </select>
+                        </p>
+                    </div>
+                    <p>Don't see the result you're looking for?</p>
+                    <Link to="add-institution">Add an institution listing here</Link>
                 </div>
-                <div>
-                    <p>
-                        Sort By Names:
-                        <select onChange={this.handleSortChange}>
-                            <option value={1}>Ascending</option>
-                            <option value={-1}>Descending</option>
+                <div className={styles.page_tools}>
+                    <div>
+                        Num Results:
+                        <select value={this.state.show} onChange={this.handleShowChange}>
+                            <option value={10}>10</option>
+                            <option value={25}>25</option>
+                            <option value={50}>50</option>
+                            <option value={100}>100</option>
                         </select>
+                    </div>
+                    <div>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.showPreviousPage}
+                        >{`< Previous ${this.state.show}`}</span>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.showNextPage}
+                        >{`Next ${this.state.show} >`}</span>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.resetControls}
+                        >
+                            Reset
+                        </span>
+                    </div>
+                </div>
+                <div className={styles.reviews}>
+                    <p>
+                        Querying {this.state.type} for: "{query}"
                     </p>
+                    <p>
+                        Showing results
+                        {` ${this.state.skip + 1} - ${Number(this.state.skip) +
+                            Number(this.state.show) +
+                            1}`}{' '}
+                        of {this.state.count}
+                    </p>
+                    {resultsList} {/* List of Reviews go here */}
                 </div>
-                <p>Don't see the result you're looking for?</p>
-                <Link to="add-institution">Add an institution listing here</Link>
-            </div>
-            <div className={styles.page_tools}>
-                <div>
-                    Num Results:
-                    <select value={this.state.show} onChange={this.handleShowChange}>
-                        <option value={10}>10</option>
-                        <option value={25}>25</option>
-                        <option value={50}>50</option>
-                        <option value={100}>100</option>
-                    </select>
+                <div className={styles.page_tools}>
+                    <div>
+                        Num Results:
+                        <select value={this.state.show} onChange={this.handleShowChange}>
+                            <option value={10}>10</option>
+                            <option value={25}>25</option>
+                            <option value={50}>50</option>
+                            <option value={100}>100</option>
+                        </select>
+                    </div>
+                    <div>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.showPreviousPage}
+                        >{`< Previous ${this.state.show}`}</span>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.showNextPage}
+                        >{`Next ${this.state.show} >`}</span>
+                        <span
+                            className={`${styles.page_nav} ${'noselect'}`}
+                            onClick={this.resetControls}
+                        >
+                            Reset
+                        </span>
+                    </div>
                 </div>
-                <div>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.showPreviousPage}>{`< Previous ${this.state.show}`}</span>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.showNextPage}>{`Next ${this.state.show} >`}</span>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.resetControls}>Reset</span>
-                </div>
-            </div>
-            <div className={styles.reviews}>
-                <p>Querying {this.state.type} for: "{query}"</p>
-                <p>Showing results
-                    {` ${this.state.skip + 1} - ${Number(this.state.skip) + Number(this.state.show) + 1}`}
-                    {" "}of {this.state.count}
-                </p>
-                {resultsList} {/* List of Reviews go here */}
-            </div>
-            <div className={styles.page_tools}>
-                <div>
-                    Num Results:
-                    <select value={this.state.show} onChange={this.handleShowChange}>
-                        <option value={10}>10</option>
-                        <option value={25}>25</option>
-                        <option value={50}>50</option>
-                        <option value={100}>100</option>
-                    </select>
-                </div>
-                <div>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.showPreviousPage}>{`< Previous ${this.state.show}`}</span>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.showNextPage}>{`Next ${this.state.show} >`}</span>
-                    <span className={`${styles.page_nav} ${"noselect"}`} onClick={this.resetControls}>Reset</span>
-                </div>
-            </div>
-        </PageTemplate >
+            </PageTemplate>
+        );
     }
 }
 
